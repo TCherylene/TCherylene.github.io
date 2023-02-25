0bf8ef35888a8b558ce155598e3e33ca75b6463e
Chery <cherylene40@gmail.com>
2023-02-25 22:02:16 +0700

    change description

--- landingpage/js/main.js
+++ landingpage/js/main.js
@@ -2,17 +2,17 @@ const education = {
     content:[{
         name: "SMAN 2 Surabaya",
         image: "logo-sman-2-surabaya.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "(2017 - 2020) I'm not very active in organization. To support the need of non-academic activities, I joined International Mathematics Olympiad (IMO) as my extracurricular which made me getting better in mathematics"
     },
     {
         name: "Sepuluh Nopember Institute of Technology",
         image: "logo-its.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "(2020 - now) Information Technology Student. I want to challenge myself by joining some organization because I never done it before. Currently I'm active at Himpunan Mahasiswa Teknologi Informasi (HMIT) as general secretary"
     },
     {
         name: "Bangkit Program 2023",
         image: "logo-bangkit.png",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "(2023 - now) By choosing Cloud Computing as my program in Bangkit 2023, I want to challenge myself to be much much better version of myself. I want to maximize the opportunity given to me by joining Bangkit Program."
     }]
 }
 
