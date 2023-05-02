3dba279e0f7330116bc5040878ddb03ed34d3a56
Chery <cherylene40@gmail.com>
2023-05-02 21:50:12 +0700

    add scroll to top button

--- landingpage/js/main.js
+++ landingpage/js/main.js
@@ -1,27 +1,53 @@
-const education = {
-    content:[{
-        name: "SMAN 2 Surabaya",
-        image: "logo-sman-2-surabaya.jpg",
-        description: "(2017 - 2020) I'm not very active in organization. To support the need of non-academic activities, I joined International Mathematics Olympiad (IMO) as my extracurricular which made me getting better in mathematics"
-    },
-    {
-        name: "Sepuluh Nopember Institute of Technology",
-        image: "logo-its.png",
-        description: "(2020 - now) Information Technology Student. I want to challenge myself by joining some organization because I never done it before. Currently I'm active at Himpunan Mahasiswa Teknologi Informasi (HMIT) as general secretary"
-    },
-    {
-        name: "Bangkit Program 2023",
-        image: "logo-bangkit.png",
-        description: "(2023 - now) By choosing Cloud Computing as my program in Bangkit 2023, I want to challenge myself to be much much better version of myself. I want to maximize the opportunity given to me by joining Bangkit Program."
-    }]
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", (e) => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+const education = {
+  content: [
+    {
+      name: "SMAN 2 Surabaya",
+      image: "logo-sman-2-surabaya.jpg",
+      description:
+        "(2017 - 2020) I'm not very active in organization. To support the need of non-academic activities, I joined International Mathematics Olympiad (IMO) as my extracurricular which made me getting better in mathematics",
+    },
+    {
+      name: "Sepuluh Nopember Institute of Technology",
+      image: "logo-its.png",
+      description:
+        "(2020 - now) Information Technology Student. I want to challenge myself by joining some organization because I never done it before. Currently I'm active at Himpunan Mahasiswa Teknologi Informasi (HMIT) as general secretary",
+    },
+    {
+      name: "Bangkit Program 2023",
+      image: "logo-bangkit.png",
+      description:
+        "(2023 - now) By choosing Cloud Computing as my program in Bangkit 2023, I want to challenge myself to be much much better version of myself. I want to maximize the opportunity given to me by joining Bangkit Program.",
+    },
+  ],
+};
 
 const educationContent = document.querySelector("#education-content");
 
 var markup = "";
 
-education.content.forEach(element => {
-    markup += `
+education.content.forEach((element) => {
+  markup += `
     <div class="education-item"> 
         <div class="logo-image">
             <img src="img/${element.image}">
@@ -31,7 +57,7 @@ education.content.forEach(element => {
             <p> ${element.description} </p>
         </div>  
     </div>
-    `
+    `;
 });
 
-educationContent.innerHTML += markup
+educationContent.innerHTML += markup;
